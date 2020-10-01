880359448e9e6459ed6fdb3cddeb400f0caa6c18
Leon Schönhoff <leon.schoenhoff.ext@andsafe.de>
2020-10-01 14:40:47 +0200

    added storage account for amis

--- modules/general/main.tf
+++ modules/general/main.tf
@@ -8,4 +8,21 @@ resource "azurerm_resource_group" "feedibus-production" {
   }
 }
 
+resource "azurerm_storage_account" "ami-storage-account" {
+  account_replication_type = "LRS"
+  account_tier = "Standard"
+  location = var.location
+  name = "ami-storage-account-feedibus-001001"
+  resource_group_name = azurerm_resource_group.feedibus-production
 
+  tags = {
+    environment = var.environment
+    tfmangaged = var.tfmanaged
+    location = var.location
+  }
+}
+
+resource "azurerm_storage_container" "ami-storage-container" {
+  name = "feedibus-production-ami-storage"
+  storage_account_name = azurerm_storage_account.ami-storage-account.name
+}
