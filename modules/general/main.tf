8b86a14aa3d3fb2cbd9557708c40cd323976ad81
Leon Schönhoff <leon.schoenhoff.ext@andsafe.de>
2020-10-02 16:40:55 +0200

    added key vault for ssh key

--- modules/general/main.tf
+++ modules/general/main.tf
@@ -21,3 +21,32 @@ resource "azurerm_storage_account" "ami-storage-account" {
     location = var.location
   }
 }
+
+data "azurerm_client_config" "current-config" {}
+
+resource "azurerm_key_vault" "feedibus-secrets" {
+  location = var.location
+  name = "feedibus-secrets"
+  resource_group_name = azurerm_resource_group.feedibus-production.name
+  sku_name = "premium"
+  tenant_id = data.azurerm_client_config.current-config.tenant_id
+
+  access_policy {
+    object_id = data.azurerm_client_config.current-config.object_id
+    tenant_id = data.azurerm_client_config.current-config.tenant_id
+
+    key_permissions = [
+    "create",
+    "get",
+    ]
+    secret_permissions = [
+    "set",
+    "get",
+    "delete",
+    ]
+  }
+  tags = {
+    environment = var.environment
+    tfmanaged = var.tfmanaged
+  }
+}
