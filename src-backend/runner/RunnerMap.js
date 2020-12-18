07786b760b973757916be2722e72147f921e97fd
Simon Hanesch <simon.hanesch@web.de>
2020-12-18 22:15:04 +0100

    Youtube Runner
    Closes #92

--- src-backend/runner/RunnerMap.js
+++ src-backend/runner/RunnerMap.js
@@ -1,10 +1,12 @@
 const RssRunner = require('./RssRunner');
 const TwitterRunner = require('./TwitterRunner');
+const YoutubeRunner = require('./YoutubeRunner');
 
 /**
  * @type Record<string, AbstractRunner>
  */
 module.exports = {
     rss: new RssRunner(),
-    twitter: new TwitterRunner()
+    twitter: new TwitterRunner(),
+    youtube: new YoutubeRunner()
 }
