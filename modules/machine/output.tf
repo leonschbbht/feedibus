91409dcb2553d6480ec023379d2bd318b686a376
Leon Schönhoff <leon.schoenhoff@gmail.com>
2020-12-22 11:12:21 +0100

    added modules outputs

--- modules/machine/output.tf
+++ modules/machine/output.tf
@@ -2,7 +2,6 @@ output "public-ip-id" {
   value = azurerm_public_ip.feedibus-public-ip.id
 }
 
-
 output "feedibus-production-vm-id" {
   value = azurerm_linux_virtual_machine.feedibus-production-virtual.id
 }
