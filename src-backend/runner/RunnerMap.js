2b6159d0f3ec787458abed512bbbc578708bf858
Simon Hanesch <simon.hanesch@web.de>
2020-12-30 18:26:59 +0100

    Add FacebookRunner

--- src-backend/runner/RunnerMap.js
+++ src-backend/runner/RunnerMap.js
@@ -2,6 +2,7 @@ const RssRunner = require('./RssRunner');
 const TwitterRunner = require('./TwitterRunner');
 const YoutubeRunner = require('./YoutubeRunner');
 const TagesschauRunner = require('./TagesschauRunner');
+const FacebookRunner = require('./FacebookRunner');
 
 /**
  * @type Record<string, AbstractRunner>
@@ -10,5 +11,6 @@ module.exports = {
     rss: new RssRunner(),
     twitter: new TwitterRunner(),
     youtube: new YoutubeRunner(),
-    tagesschau: new TagesschauRunner()
+    tagesschau: new TagesschauRunner(),
+    facebook: new FacebookRunner()
 }
