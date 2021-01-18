ed13b6ac2c8b224353ceaaef3425fc3c3b495cbf
Leon Schönhoff <leon.schoenhoff@gmail.com>
2021-01-18 10:12:18 +0100

    changed configuration for mount binding

--- ansible/roles/initial-script/templates/format.sh
+++ ansible/roles/initial-script/templates/format.sh
@@ -2,21 +2,41 @@
 
 echo "Dieses Skript wird die externe Festplatte formatieren oder einhängen"
 
+echo "Anzahl der Parameter: " $#
+
+VOLUMESIZE=
+
+if [ $# -ne 1 ];
+  then
+    echo "Größe des Volumes muss übergeben werden!"
+    exit -1
+  else
+    VOLUMESIZE=$1
+fi
+
 function createMountPoint {
-  VOLUME="/dev/sdb"
+
+  VOLUME="/dev/$(lsblk | grep "${VOLUMESIZE}G" | cut -c1-3)"
+  echo $1
   DEVICE_INFO=`file -Ls $VOLUME`
-  MOUNTPATH="/mnt/extension"
+  MOUNTPATH="/persistence"
 
   if [ "$DEVICE_INFO" == "$VOLUME: data" ]; then
     echo "Formatiere Laufwerk $VOLUME"
     mkfs -t xfs $VOLUME
-    docker volume create --name database --opt type=none --opt device=/mnt/extension/database --opt o=bind
+
     mount $VOLUME $MOUNTPATH
   fi
 
+  service docker start
+  docker volume create --name database --opt type=none --opt device=$MOUNTPATH/database --opt o=bind
+  service docker stop
   mkdir -p $MOUNTPATH
+  umount $MOUNTPATH > /dev/null 2>&1 || /bin/true
+  mount $VOLUME $MOUNTPATH
+  echo $VOLUME erfolgreich auf $MOUNTPATH eingehangen!
 }
 
-echo "$VOLUME /mnt/extension xfs noatime 0 0" >> /etc/fstab
+echo "$VOLUME $MOUNTPATH xfs noatime 0 0" >> /etc/fstab
 createMountPoint
-reboot
+service docker start
