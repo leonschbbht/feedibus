c326d467e06f0dfc8a464828e9603c46e05a822e
Jan Hümmelink <j.huemmelink@gmail.com>
2021-01-21 18:56:14 +0100

    first version of api connection

--- src-frontend/vue/design/Home.vue
+++ src-frontend/vue/design/Home.vue
@@ -4,7 +4,7 @@
             :items="filteredNews"
             :items-per-page.sync="itemsPerPage"
             :page="page"
-            :sort-by="sortBy.toLowerCase()"
+            :sort-by="sortBy"
             :sort-desc="sortDesc"
             hide-default-footer
         >
@@ -73,14 +73,14 @@
                         >
                             <v-btn
                                 large
-                                :class="{primary: !sortDesc, secondary: sortDesc }"
+                                :class="{ primary: !sortDesc, secondary: sortDesc }"
                                 :value="false"
                             >
                                 <v-icon>mdi-arrow-up</v-icon>
                             </v-btn>
                             <v-btn
                                 large
-                                :class="{primary: sortDesc, secondary: !sortDesc }"
+                                :class="{ primary: sortDesc, secondary: !sortDesc }"
                                 :value="true"
                             >
                                 <v-icon>mdi-arrow-down</v-icon>
@@ -92,14 +92,14 @@
             <template #default="props">
                 <Card
                     v-for="item in props.items"
-                    :key="item.title"
-                    :title="item.title"
-                    :img="item.img"
-                    :source="item.source"
-                    :date="item.date"
+                    :key="item.messageId"
+                    :title="item.headline"
+                    :img="item.imageUrl"
+                    source="Test"
+                    :date="item.formattedDate"
                     :text="item.text"
-                    :link="item.link"
-                    :categories="item.categories"
+                    :link="item.sourceUrl"
+                    :categories="[]"
                     class="newsCard"
                 />
             </template>
@@ -117,6 +117,7 @@
 
 <script>
 import Card from './components/Card.vue';
+import Api from './api.js';
 export default {
     name: 'Home',
     components: {
@@ -130,157 +131,13 @@ export default {
             sortDesc: true,
             page: 1,
             itemsPerPage: 10,
-            sortBy: 'date',
+            sortBy: 'utcTime',
             keys: [
                 { text: 'Titel', value: 'Title' },
                 { text: 'Quelle', value: 'Source' },
                 { text: 'Datum', value: 'Date' }
             ],
-            news: [
-                {
-                    title: 'Innenminister Caffier tritt zurück',
-                    source: 'Tagesschau.de',
-                    date: '1',
-                    text:
-            'Mecklenburg-Vorpommerns Innenminister Caffier ist zurückgetreten. Er war wegen eines umstrittenen Waffenkaufs unter Druck geraten. Sein Landtagsmandat will der CDU-Politiker behalten.',
-                    link: 'https://www.tagesschau.de/inland/caffier-ruecktritt-103.html',
-                    categories: ['Politik', 'Leitmedien']
-                },
-                {
-                    title: 'Geht das jetzt immer so weiter?',
-                    img:
-            'https://img.zeit.de/wissen/gesundheit/2020-11/corona-politik-impfung-lockdown-kontaktverfolgung-pandemiekontrolle-strategien-bild/wide__820x461__desktop',
-                    source: 'Zeit.de',
-                    date: '2',
-                    text:
-            ' Die Politik wird die Corona-Regeln wohl bald noch einmal verschärfen. Doch was kommt danach? Drei mögliche Langzeitstrategien, die die Politik ergreifen könnte. Eine Analyse von Jakob Simmank, Florian Schumann und Philipp Daum',
-                    link:
-            'https://www.zeit.de/wissen/gesundheit/2020-11/corona-politik-impfung-lockdown-kontaktverfolgung-pandemiekontrolle-strategien',
-                    categories: ['Corona', 'Leitmedien', 'Bla', 'Blubb', 'Kategorie1']
-                },
-                {
-                    title: 'Elektroautokäufer sollen bis 2025 Zuschuss bekommen',
-                    img:
-            'https://media1.faz.net/ppmedia/aktuell/wirtschaft/4185919099/1.7056839/format_top1_breit/vw-produktion-in-zwickau.jpg',
-                    source: 'faz.net',
-                    date: '3',
-                    text:
-            'Längere Förderung für E-Autos, zahlreiche neue Ladestationen und mehr: Die Regierung plant eine Reihe von Maßnahmen, um die deutsche Autolandschaft zu transformieren. ',
-                    link:
-            'https://www.faz.net/aktuell/wirtschaft/elektroautokaeufer-sollen-bis-2025-einen-zuschuss-kriegen-17056821.html',
-                    categories: ['Politik', 'Leitmedien', 'kategorie2']
-                },
-                {
-                    title: 'Einfach Uff: ALLE Apple M1 ausprobiert!1',
-                    img: 'http://i3.ytimg.com/vi/xSRI2_z-QwE/maxresdefault.jpg',
-                    source: 'youtube / alexibexi',
-                    date: '4',
-                    text:
-            'Das ist er also: Der M1 von Apple. Der Anfang der Zukunft von Computern! Was uns wohl bald erwartet habe ich an all den drei neuen Geräte ausprobiert. Was ist das Ergebnis? Das verrate ich euch in diesem Video! #AppleM1',
-                    link: 'https://www.youtube.com/watch?v=xSRI2_z-QwE',
-                    categories: ['Unterhaltung']
-                },
-                {
-                    title: 'Einfach Uff: ALLE Apple M1 ausprobiert!2',
-                    img: 'http://i3.ytimg.com/vi/xSRI2_z-QwE/maxresdefault.jpg',
-                    source: 'youtube / alexibexi',
-                    date: '5',
-                    text:
-            'Das ist er also: Der M1 von Apple. Der Anfang der Zukunft von Computern! Was uns wohl bald erwartet habe ich an all den drei neuen Geräte ausprobiert. Was ist das Ergebnis? Das verrate ich euch in diesem Video! #AppleM1',
-                    link: 'https://www.youtube.com/watch?v=xSRI2_z-QwE',
-                    categories: ['Unterhaltung']
-                },
-                {
-                    title: 'Einfach Uff: ALLE Apple M1 ausprobiert!3',
-                    img: 'http://i3.ytimg.com/vi/xSRI2_z-QwE/maxresdefault.jpg',
-                    source: 'youtube / alexibexi',
-                    date: '6',
-                    text:
-            'Das ist er also: Der M1 von Apple. Der Anfang der Zukunft von Computern! Was uns wohl bald erwartet habe ich an all den drei neuen Geräte ausprobiert. Was ist das Ergebnis? Das verrate ich euch in diesem Video! #AppleM1',
-                    link: 'https://www.youtube.com/watch?v=xSRI2_z-QwE',
-                    categories: ['Unterhaltung']
-                },
-                {
-                    title: 'Einfach Uff: ALLE Apple M1 ausprobiert!4',
-                    img: 'http://i3.ytimg.com/vi/xSRI2_z-QwE/maxresdefault.jpg',
-                    source: 'youtube / alexibexi',
-                    date: '7',
-                    text:
-            'Das ist er also: Der M1 von Apple. Der Anfang der Zukunft von Computern! Was uns wohl bald erwartet habe ich an all den drei neuen Geräte ausprobiert. Was ist das Ergebnis? Das verrate ich euch in diesem Video! #AppleM1',
-                    link: 'https://www.youtube.com/watch?v=xSRI2_z-QwE',
-                    categories: ['Unterhaltung']
-                },
-                {
-                    title: 'Einfach Uff: ALLE Apple M1 ausprobiert!5',
-                    img: 'http://i3.ytimg.com/vi/xSRI2_z-QwE/maxresdefault.jpg',
-                    source: 'youtube / alexibexi',
-                    date: '8',
-                    text:
-            'Das ist er also: Der M1 von Apple. Der Anfang der Zukunft von Computern! Was uns wohl bald erwartet habe ich an all den drei neuen Geräte ausprobiert. Was ist das Ergebnis? Das verrate ich euch in diesem Video! #AppleM1',
-                    link: 'https://www.youtube.com/watch?v=xSRI2_z-QwE',
-                    categories: ['Unterhaltung']
-                },
-                {
-                    title: 'Einfach Uff: ALLE Apple M1 ausprobiert!6',
-                    img: 'http://i3.ytimg.com/vi/xSRI2_z-QwE/maxresdefault.jpg',
-                    source: 'youtube / alexibexi',
-                    date: '9',
-                    text:
-            'Das ist er also: Der M1 von Apple. Der Anfang der Zukunft von Computern! Was uns wohl bald erwartet habe ich an all den drei neuen Geräte ausprobiert. Was ist das Ergebnis? Das verrate ich euch in diesem Video! #AppleM1',
-                    link: 'https://www.youtube.com/watch?v=xSRI2_z-QwE',
-                    categories: ['Unterhaltung']
-                },
-                {
-                    title: 'Einfach Uff: ALLE Apple M1 ausprobiert!7',
-                    img: 'http://i3.ytimg.com/vi/xSRI2_z-QwE/maxresdefault.jpg',
-                    source: 'youtube / alexibexi',
-                    date: '10',
-                    text:
-            'Das ist er also: Der M1 von Apple. Der Anfang der Zukunft von Computern! Was uns wohl bald erwartet habe ich an all den drei neuen Geräte ausprobiert. Was ist das Ergebnis? Das verrate ich euch in diesem Video! #AppleM1',
-                    link: 'https://www.youtube.com/watch?v=xSRI2_z-QwE',
-                    categories: ['Unterhaltung']
-                },
-                {
-                    title: 'Einfach Uff: ALLE Apple M1 ausprobiert!8',
-                    img: 'http://i3.ytimg.com/vi/xSRI2_z-QwE/maxresdefault.jpg',
-                    source: 'youtube / alexibexi',
-                    date: '11',
-                    text:
-            'Das ist er also: Der M1 von Apple. Der Anfang der Zukunft von Computern! Was uns wohl bald erwartet habe ich an all den drei neuen Geräte ausprobiert. Was ist das Ergebnis? Das verrate ich euch in diesem Video! #AppleM1',
-                    link: 'https://www.youtube.com/watch?v=xSRI2_z-QwE',
-                    categories: ['Unterhaltung']
-                },
-                {
-                    title: 'Einfach Uff: ALLE Apple M1 ausprobiert!9',
-                    img: 'http://i3.ytimg.com/vi/xSRI2_z-QwE/maxresdefault.jpg',
-                    source: 'youtube / alexibexi',
-                    date: '12',
-                    text:
-            'Das ist er also: Der M1 von Apple. Der Anfang der Zukunft von Computern! Was uns wohl bald erwartet habe ich an all den drei neuen Geräte ausprobiert. Was ist das Ergebnis? Das verrate ich euch in diesem Video! #AppleM1',
-                    link: 'https://www.youtube.com/watch?v=xSRI2_z-QwE',
-                    categories: ['Unterhaltung']
-                },
-                {
-                    title: 'Einfach Uff: ALLE Apple M1 ausprobiert!10',
-                    img: 'http://i3.ytimg.com/vi/xSRI2_z-QwE/maxresdefault.jpg',
-                    source: 'youtube / alexibexi',
-                    date: '13',
-                    text:
-            'Das ist er also: Der M1 von Apple. Der Anfang der Zukunft von Computern! Was uns wohl bald erwartet habe ich an all den drei neuen Geräte ausprobiert. Was ist das Ergebnis? Das verrate ich euch in diesem Video! #AppleM1',
-                    link: 'https://www.youtube.com/watch?v=xSRI2_z-QwE',
-                    categories: ['Unterhaltung']
-                },
-                {
-                    title: 'Einfach Uff: ALLE Apple M1 ausprobiert!11',
-                    img: 'http://i3.ytimg.com/vi/xSRI2_z-QwE/maxresdefault.jpg',
-                    source: 'youtube / alexibexi',
-                    date: '14',
-                    text:
-            'Das ist er also: Der M1 von Apple. Der Anfang der Zukunft von Computern! Was uns wohl bald erwartet habe ich an all den drei neuen Geräte ausprobiert. Was ist das Ergebnis? Das verrate ich euch in diesem Video! #AppleM1',
-                    link: 'https://www.youtube.com/watch?v=xSRI2_z-QwE',
-                    categories: ['Unterhaltung']
-                }
-            ]
+            news: []
         };
     },
     computed: {
@@ -291,7 +148,7 @@ export default {
             }
             return this.news.filter((newsItem) =>
                 newsItem.categories.some(function (e) {
-                    console.log(e)
+                    console.log(e);
                     return filter.includes(e);
                 })
             );
@@ -300,6 +157,9 @@ export default {
             return Math.ceil(this.filteredNews.length / this.itemsPerPage);
         }
     },
+    created () {
+        this.loadData();
+    },
     methods: {
         remove (id) {
             const index = this.filter.indexOf(id);
@@ -307,7 +167,16 @@ export default {
             this.filter = [...this.filter];
         },
         changed () {
-            this.$emit('changed', this.filter)
+            this.$emit('changed', this.filter);
+        },
+        async loadData () {
+            const news = await Api.messages();
+            this.news = news.map((newsItem) => {
+                newsItem.date = new Date(newsItem.time);
+                newsItem.formattedDate = newsItem.date.toLocaleDateString('de-DE', { hour: '2-digit', minute: '2-digit' });
+                newsItem.utcTime = newsItem.date.getTime();
+                return newsItem;
+            });
         }
     }
 };
@@ -316,10 +185,10 @@ export default {
 .newsCard {
   margin: 50px;
 }
-.active{
-color: accent;
+.active {
+  color: accent;
 }
-.inactive{
-    color:secondary;
+.inactive {
+  color: secondary;
 }
 </style>
